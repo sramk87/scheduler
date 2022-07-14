1bb3d3740912a7442133c4656421b4e0b8169aee
sramk87 <sramkissoon87@gmail.com>
2022-07-14 18:56:38 -0400

    color coding based on time updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,20 @@
-function timeTrack() {
-    var now = moment().hour()
-    
-    $(".description").each(function () {
-        var hour = parseInt($(this).attr("id").split("hour")[1]);
-    
-        if (hour === now) {
-            $(this).removeClass("past")
-            $(this).removeClass("future")
-            $(this).addClass("present")
-        }
-    
-        else if (hour > now) {
-            $(this).removeClass("present")
-            $(this).removeClass("future")
-            $(this).addClass("past")
-        }
-        
-        else if (hour < now) {
-            $(this).removeClass("future")
-            $(this).removeClass("present")
-            $(this).addClass("past")
-        }
-    timeTrack();
-    })}
+var blockHr = new Date();
+var hours = blockHr.getHours();
+    if(hours >= 9 && hours <= 17){
+        $(".row").addClass("present")
+        $(".row").removeClass("past")
+        $(".row").removeClass("future")
+    }
+    else if(hours < 9){
+        $(".row").addClass("past")
+        $(".row").removeClass("present")
+        $(".row").removeClass("future")
+    }
+    else{
+        $(".row").addClass("future")
+        $(".row").removeClass("past")
+        $(".row").removeClass("present")
+    } 
 
 $(document).ready(function () {
     $("#currentDay").text(moment().format("MMMM Do, YYYY"))
@@ -32,13 +24,13 @@ $(document).ready(function () {
         localStorage.setItem(time, des);
     })
 
-    $("#9am .description").val(localStorage.getItem("9am"))
-    $("#10am .description").val(localStorage.getItem("10am"))
-    $("#11am .description").val(localStorage.getItem("11am"))
-    $("#12pm .description").val(localStorage.getItem("12pm"))
-    $("#1pm .description").val(localStorage.getItem("1pm"))
-    $("#2pm .description").val(localStorage.getItem("2pm"))
-    $("3pm .description").val(localStorage.getItem("3pm"))
-    $("#4pm .description").val(localStorage.getItem("4pm"))
-    $("#5pm .description").val(localStorage.getItem("5pm"))
+    $("#9 .description").val(localStorage.getItem("9"))
+    $("#10 .description").val(localStorage.getItem("10"))
+    $("#11 .description").val(localStorage.getItem("11"))
+    $("#12 .description").val(localStorage.getItem("12"))
+    $("#13 .description").val(localStorage.getItem("13"))
+    $("#14 .description").val(localStorage.getItem("14"))
+    $("#15 .description").val(localStorage.getItem("15"))
+    $("#16 .description").val(localStorage.getItem("16"))
+    $("#17 .description").val(localStorage.getItem("17"))
 })
