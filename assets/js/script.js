4daa858fc874178d6d316fb4166dffd4bb8c08bb
sramk87 <sramkissoon87@gmail.com>
2022-07-13 22:32:52 -0400

    columns added

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
     $("#currentDay").text(moment().format("MMMM Do, YYYY"))
     $(".saveBtn").on("click", function () {
         var des = $(this).siblings(".description").val()
-        var time = $(this).parent().Attr("id")
+        var time = $(this).parent().attr("id")
         localStorage.setItem(time, des);
     })
 
